b361bc12621a7e579f0be8897ebfa553a093016b
Andrey Lementa <andrejkudrasov79070@gmail.com>
2023-07-01 11:10:02 +0300

    Descending Order

--- index.js
+++ index.js
@@ -1,9 +1,19 @@
-function longest(s1, s2) {
-  // your code
-  const arr1 = s1.split("");
-  const arr2 = s2.split("");
-  const arr3 = [...arr1, ...arr2].sort();
-  const arr4 = new Set(arr3);
-  const arr5 = [...arr4];
-  return arr5.join("");
+//function longest(s1, s2) {
+//  // your code
+//  const arr1 = s1.split("");
+//  const arr2 = s2.split("");
+//  const arr3 = [...arr1, ...arr2].sort();
+//  const arr4 = new Set(arr3);
+//  const arr5 = [...arr4];
+//  return arr5.join("");
+//}
+
+function descendingOrder(n) {
+  const str = String(n);
+
+  const arr = str.split("").sort((a, b) => (a < b ? 1 : -1));
+  const num = +arr.join("");
+  return num;
 }
+
+descendingOrder(42145);
